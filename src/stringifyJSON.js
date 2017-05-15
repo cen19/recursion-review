ec9d20e810c538c1229f77d58ea4440ad1a6522f
Hack Reactor Student <student@hackreactor.com>
2017-05-15 11:52:16 -0700

    stringify solved

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  if (Array.isArray(obj)) {
+    var output = [];
+    if (obj[0] === undefined) {
+      return '[]';
+    } else {
+      obj.forEach(function (element) {
+        output.push(stringifyJSON(element));
+      });
+    }
+
+    return '[' + output.join(',') + ']';    
+  }
+  
+  if (typeof obj === 'object' && obj) {
+    var output = [];
+    for (var key in obj) {
+      if ( typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      } else {
+        output.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+
+    return '{' + output.join(',') + '}';
+  }
+
+
+
+  return '' + obj;
+
 };
+
