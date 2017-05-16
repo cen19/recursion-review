574e5f906a53bdd4f27283d2f84d0135a3dce129
Hack Reactor Student <student@hackreactor.com>
2017-05-16 14:25:25 -0700

    progress on parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,12 +1,44 @@
-// this is what you would do if you were one to do things the easy way:
-// var parseJSON = JSON.parse;
+// debugger;
+var changeElements = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (Number(array[i])) {
+      array[i] = Number(array[i]);
+    } else if (array[i] === 'true') {
+      array[i] = true;
+    } else if (array[i] === 'false') {
+      array[i] = false;
+    } else {
+      array[i] = array[i].slice(1, array[i].length - 1);
+    }
+  }
+//     array.forEach(function (element) {
+//       if (Number(element)) {
+//           element = Number(element);
+//       } else if (element === 'true') {
+//           element = true;
+//       } else {
+//           element = element.slice(1, element.length - 1);
+//       }
+//     });
 
-// but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-  if (json[0] === '[') {
-    return [];
-  }
-  if (json[0] === '{') {
-    return [];
-  }
 };
+
+var parseJSON = function(json) {
+
+//   if (json[0] === '[') {
+    // if that first index of the string is the '[', the rest should be an array
+  var arrayified = json.slice(1, json.length - 1).split(',');
+     //debugger;
+//     return arrayified.map(function (element) {
+//       changeElements(element);
+//     });
+//   }
+  changeElements(arrayified);
+  return arrayified;
+};
+
+
+var testArray = [true, 5, false];
+var stringTestArray = JSON.stringify(testArray);
+parseJSON(stringTestArray);
+
